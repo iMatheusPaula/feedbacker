c4c83028c26e06ac54b83cd16afb7f697c5f4953
iMatheusPaula <imatheuspaula@gmail.com>
2024-05-27 21:21:15 -0300

    new route for check apikey

--- laravel/app/Http/Controllers/UserTokenController.php
+++ laravel/app/Http/Controllers/UserTokenController.php
@@ -16,4 +16,14 @@ class UserTokenController extends Controller
         User::where('id', $idUser)->update(['api_token' => Str::random(60)]);
         return response()->json(Response::HTTP_OK);
     }
+    public function checkExistence(string $apikey): \Illuminate\Http\JsonResponse
+    {
+        $apiKeyT = filter_var($apikey,FILTER_SANITIZE_SPECIAL_CHARS);
+        if(User::all()->where('api_token', $apiKeyT)->count()){
+            return response()->json(null,Response::HTTP_OK);
+        }
+        else{
+            return response()->json(null,Response::HTTP_NOT_FOUND);
+        }
+    }
 }
